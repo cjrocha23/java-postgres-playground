d43b157ed3931bcd8a9f6ce42cb4b98a5853efa1
Claudio Rocha <cjrocha23@gmail.com>
2023-08-01 12:15:37 +0000

    Inclusão AppMatrizes.

---
+++ src/main/java/com/example/AppMatrizes.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppMatrizes {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("### matrizes JAVA ###");
+        System.out.println();
+
+        double matriz[][] = { {140, 150}, {160, 170} };
+        System.out.println(Arrays.toString(matriz[0]));
+        //System.out.println(Arrays.toString(matriz[0]));
+        System.out.println(Arrays.toString(matriz[1]));
+        
+        System.out.println();
+        
+    }
+}
