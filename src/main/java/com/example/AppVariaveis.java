89ceda48772f90de8214828d7eed49ac7185d111
Claudio Rocha <cjrocha23@gmail.com>
2023-08-01 12:12:51 +0000

    Inclusão AppVariáveis.

---
+++ src/main/java/com/example/AppVariaveis.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class AppVariaveis {
+    public static void main(String[] args) {
+        
+        System.out.println("");
+        System.out.println("### Variáveis JAVA ###");
+        System.out.println("");
+        
+        var idade = 55;
+        var peso = 80;
+        System.out.println("Idade: " + idade + ".");
+        System.out.println("Peso: " + peso + ".");
+        var sexo = "M";
+        if (sexo == "M")
+            System.out.println("Sexo: Masculino.");
+        else
+            System.out.println("Sexo: Feminino.");     
+        
+        System.out.println("");
+    }
+}
