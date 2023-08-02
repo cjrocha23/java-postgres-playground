508e082ddfd0d7e4d6852bc875a3adfc8752ea0e
Claudio Rocha <cjrocha23@gmail.com>
2023-08-02 13:06:51 +0000

    Inclusão classe AppOperadores.

---
+++ src/main/java/com/example/AppOperadores.java
@@ -0,0 +1,33 @@
+package com.example;
+
+public class AppOperadores {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("### Operadores JAVA. ###");
+        System.out.println();
+        // "+", "-", "/", "*", "%", "++", "--"
+
+        double x = 9.4;
+        double y = 2.3;
+        double divisao;
+        double resto;
+        divisao = x / y;
+        resto = x % y;
+        System.out.println("Operação.: ("+ x +" / "+ y +")");
+        System.out.println("Resultado: " + Double.toString(divisao));
+        System.out.println("Resto....: " + Double.toString(resto));
+        System.out.println();
+
+        System.out.println();
+        System.out.println("### Operadores de Atribuições Compostas JAVA. ###");
+        System.out.println("### Operadores de Atribuições Compostas: '+=', '-=', '*=', '/=', '%='");
+        System.out.println();
+        
+        System.out.println();
+        System.out.println("### Operadores Relacionais JAVA. ###");
+        System.out.println("Operadores Relacionais: '+=', '-=', '*=', '/=', '%='");
+        System.out.println();
+
+    }
+}
