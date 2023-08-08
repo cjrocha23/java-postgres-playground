eda7522d5e359bc95f104599211658c110359072
Claudio Rocha <cjrocha23@gmail.com>
2023-08-08 02:03:48 +0000

    Inclusão da segunda parte TDD. Não consta a última aula de TDD.

--- src/main/java/com/example/MathUtil.java
+++ src/main/java/com/example/MathUtil.java
@@ -1,7 +1,30 @@
 package com.example;
 
 public class MathUtil {
-    int mdc(int a, int b) {
-        return 0;
+    static int mdc(int a, int b) {
+
+        //System.out.println("Numeros pré-math: " + Integer.toString(a) + " e " + Integer.toString(b));
+        a = Math.abs(a);
+        b = Math.abs(b);
+        
+        int maiorValor = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maiorValor;
+        //System.out.println("Numeros pós-math: " + Integer.toString(a) + " e " + Integer.toString(b));
+
+        if (b > 0 && a % b == 0) {
+            return b;
+        }
+
+        if (b == 0) {
+            //return a * -1;
+            return Math.abs(a);
+        }
+
+        //return -1;
+        //throw new UnsupportedOperationException("Não é possível calcular o MDC dos valores: \n" + Integer.toString(a) + " e " + Integer.toString(b) + ",");
+        //return 1;
+        return mdc(a - b, b);
+
     }
 }
--- src/test/java/com/example/MathUtilTest.java
+++ src/test/java/com/example/MathUtilTest.java
@@ -6,10 +6,107 @@ import org.junit.jupiter.api.Test;
 
 public class MathUtilTest {
     @Test
-    void testMdc() {
+    void testMdcProp01Imp() {
         int a = 6, b = 3;
         int esperado = 3;
         int obtido = MathUtil.mdc(a, b);
         assertEquals(obtido, esperado);
     }
+
+    @Test
+    void testMdcProp01Par() {
+        int a = 6, b = 2;
+        int esperado = 2;
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp03Neg() {
+        int a = -6, b = 0;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp03Pos() {
+        int a = 6, b = 0;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp06Cre() {
+        int a = 2, b = 6;
+        int esperado = MathUtil.mdc(b, a);
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp06Dec() {
+        int a = 6, b = 2;
+        int esperado = MathUtil.mdc(b, a);
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp07 () {
+        int a = 6, b = 2;
+        int esperado = MathUtil.mdc(-a, b);
+        int obtido = MathUtil.mdc(-a, -b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp08Pos () {
+        int a = 6;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, a);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp08Neg () {
+        int a = -6;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, a);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp12Prix2 () {
+        int b = 7, a = b;
+        int esperado = b;
+        int obtido = MathUtil.mdc(b, a);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp12Prix1 () {
+        int b = 7, a = 2;
+        int esperado = 1;
+        int obtido = MathUtil.mdc(b, a);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcCasoGeral () {
+        int a = 30, b = 12;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
+    @Test
+    void testMdcProp04 () {
+        int m = 2, a = 6, b = 3;
+        int esperado = MathUtil.mdc(m * a, m * b);
+        int obtido = m * MathUtil.mdc(a, b);
+        assertEquals(obtido, esperado);
+    }
+
 }
